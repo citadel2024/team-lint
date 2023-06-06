d33a156517cbdb436b06db034252c217b7228d01
dkkb <dabkb@aol.com>
2023-06-06 15:18:40 +0800

    feat(init): init project

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+
+ESlint config, Prettier config, Remark config. Fork from <https://github.com/lobehub/lobe-lint>.
+
+</div>
---
+++ src/semantic-release/index.ts
@@ -0,0 +1,10 @@
+import type { Options } from 'semantic-release';
+
+import createConfig from './createConfig';
+
+const defaultConfig: Options = createConfig();
+
+export default {
+    $schema: 'https://json.schemastore.org/semantic-release',
+    ...defaultConfig,
+};
